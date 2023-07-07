67bf690a3a6c2165060258ff89d8b4d02b9651c2
Brian Irwin <32822229+0x4249@users.noreply.github.com>
2023-07-07 12:13:19 -0700

    Improve structure of src/samplers/__init__.py

--- src/samplers/__init__.py
+++ src/samplers/__init__.py
@@ -19,34 +19,34 @@ def get_sampler(sampler_dict):
     sampler_name = sampler_dict["name"]
     
     
-    if sampler_name in ["zero_sampler"]:
+    if sampler_name in ["zero sampler"]:
         sampler = scalar_samplers.zero_sampler()
         
         
-    elif sampler_name in ["unit_sampler"]:
+    elif sampler_name in ["unit sampler"]:
         sampler = scalar_samplers.unit_sampler()
     
     
-    elif sampler_name in ["symmetric_uniform_random_sampler"]:
+    elif sampler_name in ["symmetric uniform random sampler"]:
         r = sampler_dict["radius"]
         sampler = scalar_samplers.symmetric_uniform_random_sampler(epsilon=r)
         
         
-    elif sampler_name in ["gaussian_random_sampler"]:
+    elif sampler_name in ["gaussian random sampler"]:
         mu = sampler_dict["mu"]
         sigma = sampler_dict["sigma"]
         sampler = scalar_samplers.gaussian_random_sampler(mu=mu,
                                                           sigma=sigma)
     
     
-    elif sampler_name in ["spherical_ball_uniform_random_sampler"]:
+    elif sampler_name in ["spherical ball uniform random sampler"]:
         d = sampler_dict["dimension"]
         r = sampler_dict["radius"]
         sampler = vector_samplers.spherical_ball_uniform_random_sampler(d=d,
                                                                         r=r)
         
         
-    elif sampler_name in ["spherical_surface_uniform_random_sampler"]:
+    elif sampler_name in ["spherical surface uniform random sampler"]:
         d = sampler_dict["dimension"]
         r = sampler_dict["radius"]
         sampler = vector_samplers.spherical_surface_uniform_random_sampler(d=d,
@@ -66,21 +66,20 @@ def get_vector_sampler(sampler_dict):
     Returns:
         sampler (callable): function that generates samples of specified type.
     """
-    
     sampler_name = sampler_dict["name"]
-    dim = sampler_dict["dimension"]
-    sigma = sampler_dict.get("sigma", None)
     
     
-    if sampler_name in ["spherical_ball_uniform_random_sampler"]:
+    if sampler_name in ["spherical ball uniform random sampler"]:
+        dim = sampler_dict["dimension"]
         sampler = vector_samplers.spherical_ball_uniform_random_sampler(d=dim)
         
         
-    elif sampler_name in ["spherical_surface_uniform_random_sampler"]:
+    elif sampler_name in ["spherical surface uniform random sampler"]:
+        dim = sampler_dict["dimension"]
         sampler = vector_samplers.spherical_surface_uniform_random_sampler(d=dim)   
     
     
-    elif sampler_name in ["standard_multivariate_normal_random_sampler"]:
+    elif sampler_name in ["standard multivariate normal random sampler"]:
         sampler = vector_samplers.multivariate_normal_random_sampler(mu=torch.zeros(dim),
                                                                      Sigma=torch.eye(dim))
     
